7fa7039c3c37e107f7f96aba87c4f3892133e4c0
Christopher Sampon <cjsampon@uwm.edu>
2015-10-29 17:59:04 -0500

    Added __init__ file to templates folder. Added templates folder to TEMPLATES.DIRS in settings.

--- Drinklet/settings.py
+++ Drinklet/settings.py
@@ -59,7 +59,7 @@ ROOT_URLCONF = 'Drinklet.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR+'/templates/'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
