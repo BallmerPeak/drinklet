b0f797983ddfabe95fb6c549f861ef32b8768f90
Christopher Sampon <cjsampon@uwm.edu>
2015-10-31 13:29:27 -0500

    Implemented Ingredients page behavior (except search). Created a static ingredient files directory with the index's js file. Checking an ingredient's checkbox will create a corresponding tag chip. Unchecking an ingredient's checkbox will remove its tag chip. Clicking x on a tag chip will uncheck its ingredient's checkbox. Clicking clear will remove all tag chips and uncheck all ingredients' checkboxes.

---
+++ ingredients/static/ingredients/index.js
@@ -0,0 +1,194 @@
+// Global list of ingredient objects
+var ingredientList = [];
+
+/**
+ * @method getIngredientIDList
+ * Retrieve an array of just ingredient ids.
+ * @returns {Array} Array of ingredient ids.
+ */
+function getIngredientIDList() {
+	var idList = [];
+	// Build the id list according to ingredient list
+	for(var i=0; i<ingredientList.length; i++) {
+		// Grab the ingredient id and push into the id list
+		idList.push(ingredientList[i].id);
+	}
+	// Return the list of ingredient ids
+	return idList;
+}
+/**
+ * @method getIngredientIndex
+ * Get the index of an ingredient in the ingredient list.
+ * @param  {Number} _id The id of the ingredient to find
+ * @returns {Number} The index of the ingredient or -1 if not found.
+ */
+function getIngredientIndex(_id) {
+	// Check if the id exists and is a number
+	if(_id !== undefined
+	&& !isNaN(_id)) {
+		// Find the index of the ingredient in the ingredient list
+		for(var i=0; i<ingredientList.length; i++) {
+			// Return index of the first occurrence of the ingredient
+			if(parseInt(ingredientList[i].id) === parseInt(_id)) return i;
+		}
+	}
+	// Assume ingredient not found or invalid id passed in
+	return -1;
+}
+
+/**
+ * @makeChip
+ * Create a deletable chip corresponding to an ingredient.
+ * @param   {Object}   _data     The ingredient object to make a chip of
+ * @param   {Function} _callback The click handler for when the X is clicked
+ * @returns {Element}  The chip DOM element or null if ingredient not found.
+ */
+function makeChip(_data,_callback) {
+	// Check if the data exists and is an object
+	if(_data !== undefined
+	&& toString.call(_data) === '[object Object]') {
+		// Create the DOM elements for a materialize chip
+		var chip = document.createElement('div'),
+			text = document.createTextNode(_data.name),
+			icon = document.createElement('i'),
+			type = document.createTextNode('close');
+		
+		// Add the classes to the DOM elements
+		chip.className = 'chip';
+		icon.className = 'material-icons';
+
+		// Set up the structure of the chip
+		icon.appendChild(type);
+		chip.appendChild(text);
+		chip.appendChild(icon);
+
+		// Add a custom property for tracking which ingredient
+		icon._id = _data.id;
+
+		// Add the click handler for the chip
+		icon.addEventListener('click', function(_e) {
+			// Check if the callback exists and is a function
+			if(_callback !== undefined
+			&& toString.call(_callback) === '[object Function]') {
+				// Call the callback, passing it the custom id property
+				_callback(_e.srcElement._id);
+			}
+		});
+
+		// Return the newly created chip element
+		return chip;
+	}
+	// Assume invalid ingredient passed in
+	return null;
+}
+/**
+ * @method updateInventory
+ * Given what is in the list of ingredients, update the DOM element
+ * "inventory" with deletable chips.
+ */
+function updateInventory() {
+	// Grab the inventory DOM element
+	var inventory = document.getElementById('inventory'),
+		buttons = document.getElementById('inventoryButtons'),
+		emptyMessage = document.getElementById('inventoryEmpty');
+
+	// Check if the inventory DOM element exists
+	if(inventory) {
+		// Clear the inventory DOM element
+		inventory.innerHTML = '';
+
+		// Create a chip for each ingredient in the ingredient list
+		for(var i=0; i<ingredientList.length; i++) {
+			// Call the makeChip function with the ingredient and click handler
+			var chip = makeChip(ingredientList[i], function(_id) {
+				var checkBox,
+					ingredientIndex;
+				if(_id !== undefined) {
+					// If the ingredient id was passed in, grab its checkbox
+					checkBox = document.getElementById(_id+'_ingredient');
+
+					// Find the index of the ingredient in the ingredient list
+					ingredientIndex = getIngredientIndex(_id);
+
+					// Check if the ingredient was found and has a checkbox
+					if(ingredientIndex !== undefined 
+					&& checkBox) {
+						// Uncheck the ingredient's checkbox
+						checkBox.checked = false;
+
+						// Remove the ingredient from the ingredient list
+						ingredientList.splice(ingredientIndex,1);
+					}
+				}
+
+				// Update the DOM with the list of ingredients
+				updateInventory();
+			});
+
+			// Add the chip to the inventory DOM element
+			if(chip) inventory.appendChild(chip);
+		}
+		// Check if the inventoryButtons and inventoryEmpty DOM elements exist
+		if(buttons
+		&& emptyMessage) {
+			// If there are ingredients, show the buttons and hide the empty message
+			if(ingredientList.length > 0) {
+				buttons.style.display = 'inline';
+				emptyMessage.style.display = 'none';
+
+			// Otherwise hide the buttons and show the empty message
+			} else {
+				buttons.style.display = 'none';
+				emptyMessage.style.display = 'inline';
+			}	
+		}
+	}
+}
+/**
+ * @method updateIngredient
+ * Either adds or removes an ingredient from the selected ingredient list.
+ * @param {Object} _ingredient The ingredient to update
+ */
+function updateIngredient(_ingredient) {
+	// Make sure the data passed in is a non null object
+	if(_ingredient !== undefined
+	&& toString.call(_ingredient) === '[object Object]') {
+		// Find the index of the ingredient in the ingredient list
+		var ingredientIndex = getIngredientIndex(_ingredient.id);
+
+		// If the ingredient was in the list, remove it
+		if(ingredientIndex >= 0) ingredientList.splice(ingredientIndex,1);
+
+		// Otherwise add it to the list
+		else ingredientList.push(_ingredient);
+
+		// Update the DOM with the list of ingredients
+		updateInventory();
+	}
+}
+/**
+ * @method clearIngredients
+ * Unchecks all selected ingredients and clears ingredient list.
+ */
+function clearIngredients() {
+	// Uncheck each currently selected ingredient's checkbox
+	for(var i=0; i<ingredientList.length; i++) {
+		// Grab the ingredient's checkbox
+		var checkBox = document.getElementById(ingredientList[i].id+'_ingredient');
+		// Uncheck the checkbox
+		if(checkBox) checkBox.checked = false;
+	}
+	// Clear the ingredient list
+	ingredientList = [];
+
+	// Update the DOM with the list of ingredients
+	updateInventory();
+}
+/**
+ * @method performSearch
+ * Grabs the list of ingredient ids and sends them to server.
+ */
+function performSearch() {
+	// Currently, just alerting the list of ids
+	alert(JSON.stringify(getIngredientIDList()));
+}
