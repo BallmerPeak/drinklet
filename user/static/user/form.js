eafc1f599dd5adfa9420fc5c44ba433c358492e8
Christopher Sampon <cjsampon@uwm.edu>
2015-10-31 16:53:06 -0500

    Implemented login and register forms. Added Login and Register buttons to the navbar. Moved the currently existing login and register template elements to the user templates as they directly relate to users. Login and Register templates are both modals now that flyin from the bottom. Removed the search bar from the navbar.

---
+++ user/static/user/form.js
@@ -0,0 +1,11 @@
+$(document).ready(function(){
+	// Open the modal
+	$('.modal-trigger').leanModal({
+		// Modal complete event handler
+		complete: function() { 
+			// Remove all overlays
+			$('.lean-overlay').remove();
+		 }	
+	});
+
+});
