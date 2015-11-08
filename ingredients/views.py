179a115d5ffcd662f4a0c825be07b9f76baf30e9
Bradley Eggert <breggert@uwm.edu>
2015-11-07 19:18:19 -0600

    Changes to ingredients to add selected ingredients to user ingredients

--- ingredients/views.py
+++ ingredients/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from django.views.generic import View
+from user.models import UserProfile
 import json
 
 from .models import Ingredient
@@ -13,6 +14,29 @@ class SearchOptions(View):
         for ingredient in Ingredient.objects.all().values():
             ingredientsJSON[ingredient['id']] = ingredient
         context = {
+            'categories': Ingredient.get_all_ingredients(),
+            'ingredientsJSON': json.dumps(ingredientsJSON)
+        }
+        return render(request, 'ingredients/index.html', context)
+    def post(self, request):
+        """
+        Adds selected ingredients to ingredient list
+        """
+        ingredients = request.POST.getlist('selectedIngredients')
+        profile = UserProfile.get_or_create_profile(self.request.user)
+        test = list(profile.ingredients.values_list('id', flat=True))
+        uniqueingredients = list(set(ingredients + test))
+        #UserProfile.get_or_create_profile(self.request.user).add_user_ingredients(uniqueingredients)
+        ingredientTest = self.request.user.userprofile.add_user_ingredients(uniqueingredients)
+        userIngredientsJSON = {}
+        for useringredient in ingredientTest.keys():
+            userIngredientsJSON[useringredient] = useringredient
+
+        ingredientsJSON = {}
+        for ingredient in Ingredient.objects.all().values():
+            ingredientsJSON[ingredient['id']] = ingredient
+        context = {
+            'userIngredientsJSON': json.dumps(userIngredientsJSON),
             'categories': Ingredient.get_all_ingredients(),
             'ingredientsJSON': json.dumps(ingredientsJSON)
         }
