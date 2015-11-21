7b792011de8143f93e0068912ae58770788ce7f0
Christopher Sampon <cjsampon@uwm.edu>
2015-11-21 12:45:43 -0600

    Create Recipe List (Not RESULTS list) - List Recipes - complete
    Create Recipe List (Not RESULTS list) - Pagination - complete
    Create Recipe List (Not RESULTS list) - Sort recipes by name - complete
    Added Row class for pages that list recipeRows
    Added Recipe list to navbar

--- recipes/urls.py
+++ recipes/urls.py
@@ -1,7 +1,8 @@
 from django.conf.urls import include, url
-from .views import SearchRecipes, CreateRecipe
+from .views import SearchRecipes, CreateRecipe, ListRecipes
 
 urlpatterns = [
 	url(r'^$', SearchRecipes.as_view(), name='recipes.search'),
 	url(r'^create/$', CreateRecipe.as_view(), name='recipes.create'),
+	url(r'^list/$', ListRecipes.as_view(), name='recipes.list')
 ]
